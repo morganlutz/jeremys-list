9c5b3478624b89a9e26616a04f722c9ec9bbe5c8
morgan <morgan.lutz@gmail.com>
2015-10-05 11:07:08 -0700

    move tag creation to tags.java

--- src/main/java/data/model/tags.java
+++ src/main/java/data/model/tags.java
@@ -2,6 +2,10 @@ public class Tag {
   private String description;
   private int id;
 
+  public Tag(String description) {
+    this.description = description;
+  }
+
   public int getId() {
     return id;
   }
