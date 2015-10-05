c90cd45c2c8b7ae71316c02c393c2d985e4f1327
morgan <morgan.lutz@gmail.com>
2015-10-05 10:41:48 -0700

    split Quadrant class into quadrants and quadrantDataStore

---
+++ src/main/java/data/model/quandrants.java
@@ -0,0 +1,12 @@
+public class Quadrant {
+  private int id;
+  private String quadrant;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getQuadrant() {
+    return quadrant;
+  }
+}
