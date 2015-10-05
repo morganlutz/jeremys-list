0c2563ead7dbdad012bde437995d1987d46ee43d
morgan <morgan.lutz@gmail.com>
2015-10-05 10:35:30 -0700

    split Restaurant class into restaurants and restaurantDataStore

---
+++ src/main/java/data/model/restaurants.java
@@ -0,0 +1,42 @@
+public class Restaurant {
+  private int id;
+  private String name;
+  private String address;
+  private String phone;
+  private String website;
+  private String yelp;
+  private String hours;
+  private int quadrant_id;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public String getWebsite() {
+    return website;
+  }
+
+  public String getYelp() {
+    return yelp;
+  }
+
+  public String getHours() {
+    return hours;
+  }
+
+  public int getQuadrantId() {
+    return quadrant_id;
+  }
+}
