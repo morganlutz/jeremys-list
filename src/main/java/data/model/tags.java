86ef034206aa3a8116190f28973550aa434e30f4
morgan <morgan.lutz@gmail.com>
2015-10-05 11:00:42 -0700

    split Tag class into tags and tagDataStore

---
+++ src/main/java/data/model/tags.java
@@ -0,0 +1,12 @@
+public class Tag {
+  private String description;
+  private int id;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+}
