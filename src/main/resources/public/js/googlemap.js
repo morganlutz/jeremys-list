4c85790b7a3df12786907dd652289d321be03c4b
morgan <morgan.lutz@gmail.com>
2015-10-15 12:45:54 -0700

    create markers of all restaurants in category

--- src/main/resources/public/js/googlemap.js
+++ src/main/resources/public/js/googlemap.js
@@ -1,45 +1,67 @@
 (function() {
   var geocoder;
   var map;
-  var address = [];
-
-  function initialize() {
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var initialize = function() {
+    var latlng = new google.maps.LatLng(45.5204527, -122.673812);
     var mapOptions = {
-      zoom: 3,
+      zoom: 15,
       center: latlng
     }
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
   }
 
-  // function codeAddress() {
-  //
-  //    geocoder.geocode( { 'address': address}, function(results, status) {
-  //      if (status == google.maps.GeocoderStatus.OK) {
-  //        map.setCenter(results[0].geometry.location);
-  //
-  //        var infowindow = new google.maps.InfoWindow({
-  //          content: contentString
-  //        });
-  //
-  //        var marker = new google.maps.Marker({
-  //            map: map,
-  //            Title: '' + address,
-  //            position: results[0].geometry.location
-  //        });
-  //
-  //        marker.addListener('click', function() {
-  //          infowindow.open(map, marker);
-  //
-  //
-  //        })
-  //
-  //      } else {
-  //        alert("Geocode was not successful for the following reason: " + status);
-  //      }
-  //    });
-  //  }
+  var restaurantArray = document.getElementsByClassName("restaurant-address");
+
+
+
+
+  function codeAddress(address) {
+    // debugger;
+    geocoder = new google.maps.Geocoder();
+
+     geocoder.geocode( { 'address': address }, function(results, status) {
+       if (status == google.maps.GeocoderStatus.OK) {
+         map.setCenter(results[0].geometry.location);
+
+        //  var infowindow = new google.maps.InfoWindow({
+        //    content: contentString
+        //  });
+
+         var marker = new google.maps.Marker({
+             map: map,
+             Title: '' + address,
+             position: results[0].geometry.location
+         });
+
+         marker.addListener('click', function() {
+           infowindow.open(map, marker);
+
+
+         })
+
+       } else {
+        //  alert("Geocode was not successful for the following reason: " + status);
+       }
+     });
+   }
+
+  // $(for (var i = 0; i < restaurantArray.length; i++){
+  //    debugger;
+  //    var address = restaurantArray[i].getAttribute('data-address');
+  //    $(codeAddress(address));
+  //  });
+  //  $(codeAddress);
 
   $(initialize);
+
+
+    var createMarkers = function() {
+      debugger;
+      for (var i = 0; i < restaurantArray.length; i++){
+         debugger;
+         var address = restaurantArray[i].getAttribute('data-address');
+         $(codeAddress(address));
+      }
+    };
+
  })();
